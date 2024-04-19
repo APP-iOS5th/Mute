32b0dd0bc76272894e3eb6b1ee4f308219e27f1f
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-19 09:30:28 +0900

    add: Calculator Button

--- jeongwoo/Calculator/Calculator/ContentView.swift
+++ jeongwoo/Calculator/Calculator/ContentView.swift
@@ -1,12 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let CalulatorButton = [["7","8","9","/"], ["4","5","6","*"], ["1","2","3","-"], [".","0","C","+"],["="]]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("result")
+            ForEach(CalulatorButton, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { calbtn in
+                        Button(action: {
+                            print("button")
+                        }) {
+                            Text(calbtn)
+                        }
+                    }
+                }
+            }
         }
         .padding()
     }
