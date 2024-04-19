16a97d4e7eafdb46144711fa1ff39fc8a68c7365
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-19 14:46:29 +0900

    githubimage

--- jeongwoo/PracticeSwiftUI/PracticeSwiftUI/ContentView.swift
+++ jeongwoo/PracticeSwiftUI/PracticeSwiftUI/ContentView.swift
@@ -1,30 +1,78 @@
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Button(action: {
-                doSomething()
-            }) {
-                Text("Do somthing")
-            }
-        }
-    }
-    func doSomething() {
-        print("start \(Date())")
-        Task {
-            // let result = await takesTooLong()
-            // print("DAte result: \(result)")
-            async let result = takesTooLong()
-            print("Date result: \(await result)")
-        }
-        print("End \(Date())")
+struct User: Codable {
+    let login: String
+    let avatar_url: URL
+}
+
+struct Repository: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String?
+}
+
+actor GithubService {
+    func fetchUser(username: String) async throws -> User {
+        let url = URL(string: "https://api.github.com/users/\(username)")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode(User.self, from: data)
     }
     
-    func takesTooLong() async -> Date{
-        sleep(5)
-        print("Async task completed at \(Date())")
-        return Date()
+    func fetchRepositories(username: String) async throws -> [Repository] {
+        let url = URL(string: "https://api.github.com/users/\(username)/repos")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Repository].self, from: data)
+    }
+}
+
+struct ContentView: View {
+    @State private var username = "jeongWo"
+    @State private var user: User?
+    @State private var repositories: [Repository] = []
+    let githubService = GithubService()
+    
+    var body: some View {
+        VStack {
+            TextField("Github username:", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button("Fetch Data") {
+                Task {
+                    do {
+                        async let fetchUser = githubService.fetchUser(username: username)
+                        async let fetchRepositoris = githubService.fetchRepositories(username: username)
+                        
+                        user = try await fetchUser
+                        repositories = try await fetchRepositoris
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                }
+            }
+            
+            if let user = user {
+                AsyncImage(url: user.avatar_url) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width:100, height:100)
+                .clipShape(Circle())
+                
+                Text(user.login)
+                    .font(.title)
+            }
+            
+            List(repositories) { repo in
+                VStack(alignment: .leading) {
+                    Text(repo.name)
+                        .font(.headline)
+                    Text(repo.description ?? "No description")
+                        .font(.subheadline)
+                }
+            }
+            
+        }
     }
 }
 
