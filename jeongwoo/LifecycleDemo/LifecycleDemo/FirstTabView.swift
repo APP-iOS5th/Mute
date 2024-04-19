3a314abfd6d7b860ab8749a4f0b10f28aeb4e239
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-19 17:06:41 +0900

    async task modifier

--- jeongwoo/LifecycleDemo/LifecycleDemo/FirstTabView.swift
+++ jeongwoo/LifecycleDemo/LifecycleDemo/FirstTabView.swift
@@ -2,14 +2,24 @@
 import SwiftUI
 
 struct FirstTabView: View {
+    @State var title = "View One"
     var body: some View {
-        Text("View1")
+        Text(title)
             .onAppear(perform: {
                 print("onAppear triggered")
             })
             .onDisappear(perform: {
                 print("onDisappear triggered")
+                title = "View One Again"
             })
+            .task(priority: .background) {
+                print("task trigger")
+                title = await changeTitle()
+            }
+    }
+    func changeTitle() async -> String {
+        sleep(5)
+        return "Async task complete"
     }
 }
 
