cb46856a96d8c2d211f4a1b90c8e1a2b1595769f
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-22 17:32:19 +0900

    feature: memo add

--- jeongwoo/MemoApp/MemoApp/MemoAppApp.swift
+++ jeongwoo/MemoApp/MemoApp/MemoAppApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct MemoAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Memo.self)
         }
     }
 }
