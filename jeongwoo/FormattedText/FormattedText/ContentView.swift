a532dd227e8db600f614088679e9bf5a610bff15
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-23 13:44:08 +0900

    Format Text

--- jeongwoo/FormattedText/FormattedText/ContentView.swift
+++ jeongwoo/FormattedText/FormattedText/ContentView.swift
@@ -1,12 +1,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password = "1"
+    @State private var someText = "password"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("enter your password!")
+                .fontWeight(.medium)
+            SecureField("Enter a password", text: $password)
+            Text("password entered: \(password)")
+                .italic()
+            
+            TextField("Enter some text", text: $someText)
+                .padding()
+            Text(someText)
+                .font(.largeTitle)
+                .underline()
+            
+            Text("changing text color and make it bold")
+                .foregroundStyle(.red)
+                .bold()
+            Text("Use kerning to change space btween characters in the text")
+                .kerning(4.0)
+            Text("StrikeThrough")
+                .strikethrough()
+            Text("""
+                    multilinemultilinemultilinemultilinemultilineultilinemultilinemultilinemultilinemultiline
+                """)
+            .background(.yellow)
+            .multilineTextAlignment(.leading)
+            .lineSpacing(10.0)
+            
         }
         .padding()
     }
