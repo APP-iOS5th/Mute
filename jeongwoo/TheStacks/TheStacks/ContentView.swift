67ec6fc869ae4581f50910f22ccf24e559f37f95
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-23 13:32:59 +0900

    stack

--- jeongwoo/TheStacks/TheStacks/ContentView.swift
+++ jeongwoo/TheStacks/TheStacks/ContentView.swift
@@ -10,16 +10,11 @@ struct ContentView: View {
             Text("VStack Item 3")
             HStack {
                 Text("HStack Item 1")
-                Divider()
-                    .background(.black)
-                Spacer()
                 Text("HStack Item 2")
-                Divider()
-                    .background(.black)
-                Spacer()
                 Text("HStack Item 2")
             }
-            .background(.blue)
+            .background(.red)
+            .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading)
             ZStack {
                 Text("ZStack Item 1")
                     .padding()
@@ -28,9 +23,11 @@ struct ContentView: View {
                 Text("ZStack Item 2")
                     .padding()
                     .background(.green)
-                    .offset(x: 80, y: -400)
+//                    .offset(x: 80, y: -400)
             }
+            .padding()
         }
+        .background(.brown)
     }
 }
 
