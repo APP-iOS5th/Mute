036a42330670a4c2857c23459e207e406a0e25b9
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-23 14:22:26 +0900

    add: contentview add button

--- jeongwoo/Buttons/Buttons/ContentView.swift
+++ jeongwoo/Buttons/Buttons/ContentView.swift
@@ -4,10 +4,25 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 44) {
-                NavigationLink("Button") {
+                NavigationLink("Buttons") {
                     ButtonView()
                 }
+                NavigationLink("EditButtons") {
+                    EditButtonView()
+                }
+                NavigationLink("MenuButtons") {
+                    MenuButtonView()
+                }
+                NavigationLink("PasteButtons") {
+                    PasteButtonView()
+                }
+                NavigationLink("Details about text") {
+                    Text("Very long text that should not be deplayed in a single line beacaouse it is not good design")
+                        .padding()
+                        .navigationTitle(Text("Detail"))
+                }
             }
+            .navigationTitle(Text("Main View"))
         }
     }
 }
