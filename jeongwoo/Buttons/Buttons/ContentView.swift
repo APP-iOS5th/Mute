3ca8e4ecee10ecf54bcd35d00659a0356028d6b4
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-23 14:19:53 +0900

    add: ButtonView

--- jeongwoo/Buttons/Buttons/ContentView.swift
+++ jeongwoo/Buttons/Buttons/ContentView.swift
@@ -1,15 +1,14 @@
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 44) {
+                NavigationLink("Button") {
+                    ButtonView()
+                }
+            }
         }
-        .padding()
     }
 }
 
