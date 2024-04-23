96afae949a94ac7db97fc973478202d692f7f55e
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-23 14:38:01 +0900

    feature: ButtonView

--- jeongwoo/Buttons/Buttons/ButtonView.swift
+++ jeongwoo/Buttons/Buttons/ButtonView.swift
@@ -1,18 +1,28 @@
-//
-//  ButtonView.swift
-//  Buttons
-//
-//  Created by 김정우 on 4/23/24.
-//
-
 import SwiftUI
 
 struct ButtonView: View {
+    @State var count = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome to your second view")
+            Text("Current count value: \(count)")
+            Button {
+                count += 1
+            } label: {
+                Text("Tap to Increment count")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(.blue)
+                    .clipShape(Circle())
+            }
+        }
+        .navigationTitle("Button View")
     }
 }
 
 #Preview {
-    ButtonView()
+    NavigationStack {
+        ButtonView()
+    }
 }
