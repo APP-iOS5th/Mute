61b305af26d50b4092cf24934afdfdd43f1ac4e0
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-04-24 09:28:12 +0900

    add: HostingViewController

---
+++ jeongwoo/SwiftUItoUIKit/SwiftUItoUIKit/Greetings.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct Greetings: View {
+    var textFromUIKit: String
+    var body: some View {
+        Text(textFromUIKit)
+    }
+}
+
+#Preview {
+    Greetings(textFromUIKit: "Hello, world!")
+}
