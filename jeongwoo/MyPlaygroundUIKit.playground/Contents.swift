e781fd8768525b5f6f7a0af2eb7d13c169bb8b50
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-05-07 17:43:58 +0900

    UiKit list

--- jeongwoo/MyPlaygroundUIKit.playground/Contents.swift
+++ jeongwoo/MyPlaygroundUIKit.playground/Contents.swift
@@ -62,3 +62,41 @@ if let spouseTempVar = spouseName {
 } else {
     print("No one")
 }
+
+let myRange = 10...20
+let myRange2 = 10..<20
+
+for number in myRange {
+    print(number)
+}
+
+for number in myRange2 {
+    print(number)
+}
+
+for number in (0...5).reversed() {
+    print(number)
+}
+var y = 50
+while y < 50 {
+    y += 5
+    print("y is \(y)")
+}
+
+
+//var x = 50
+//repeat {
+//    x += 5
+//    print("x is \(x)")
+//} while x < 50
+
+var shoppingList = ["Egg", "Milk"]
+print(shoppingList.count)
+shoppingList.append("Cooking Oil")
+shoppingList = shoppingList + ["Chicken"]
+shoppingList.insert("Wather", at: 1)
+
+shoppingList.remove(at: 1)
+let removeElement = shoppingList.removeLast()
+
+print("remove: \(removeElement)")
